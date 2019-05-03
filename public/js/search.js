c072a6e3c6e4c7c737fad65e0f0f03b8740cb584
Gilles <gilles.audemard@gmail.com>
2019-05-03 13:54:20 +0200

    pjax ok

--- public/js/search.js
+++ public/js/search.js
@@ -4,3 +4,6 @@ $(document).ready(function () {
         window.location.href = "/recherche/" + e.target.elements[0].value;
     });
 });
+
+
+$(document).pjax('[data-pjax] a, a[data-pjax]', '#main');
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,67 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-    <!-- Styles -->
-   <!-- <link href="{{ asset('css/style.css') }}" rel="stylesheet"> -->
+@if(request()->ajax())
+    @yield("content")
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/jquery.min.js') }}"></script>
-    <script src="{{ asset('js/jquery.pjax.js') }}"></script>
-    <script src="{{ asset('js/audio.js') }}"></script>
-    <script src="{{ asset('js/search.js') }}"></script>
-</head>
-<body>
+@else
+    @include('layouts.full')
+@endif
 
-<header>
-    <a href="{{ url('/') }}">
-        {{ config('app.name', 'Laravel') }}
-    </a>
-</header>
-
-    <!-- Authentication Links -->
-    <nav>
-        <ul>
-            @guest
-                <li><a href="{{ route('login') }}">Login</a></li>
-                <li><a href="{{ route('register') }}">Register</a></li>
-            @else
-                <li> Bonjour {{ Auth::user()->name }}</li>
-                <li><a href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                                                        document.getElementById('logout-form').submit();">
-                        Logout
-                    </a></li>
-                <li><a data-pjax href="/nouvelle">Uploader une chanson</a></li>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                    {{ csrf_field() }}
-                </form>
-
-                <script type="text/javascript">
-                    $(document).pjax('[data-pjax] a, a[data-pjax]', '#main');
-                </script>
-
-            @endguest
-        </ul>
-    </nav>
-        
-    <audio id="audio" controls>
-        <source src="" type="audio/mpeg">
-    </audio>
-
-    <form id="search">
-        <input type="search" name="search" placeholder="Votre recherche" required />
-        <input type="submit" />
-    </form>
-
-    <div id="main">
-        @yield('content')
-    </div>
-
-</body>
-</html>
